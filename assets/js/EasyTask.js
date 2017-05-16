b59bb311e204342c1668b715f7a1d3823aa15696
Avi Ihej <aviihej@gmail.com>
2017-05-15 20:56:33 -0400

    add hover functionality

--- assets/js/EasyTask.js
+++ assets/js/EasyTask.js
@@ -1,4 +1,4 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed")
 
     // //If li is grey we will turn it black
@@ -19,12 +19,12 @@ $("li").click(function(){
 });
 //Click on span to delete tasks
 
-$("span").click(function(event){
-    $(this).parent().fadeOut(500, function(){
+$("ul").on("click","span", function(event){
+    $(this).parent().fadeOut(500,function(){
         $(this).remove();
-    })
+    });
     event.stopPropagation();
-})
+});
 //EventListener for the enter key
 $("input[type='text']").keypress(function(event){
     if (event.which === 13){
@@ -33,7 +33,10 @@ $("input[type='text']").keypress(function(event){
         //get rid of task text
         $(this).val("");
         //create a new li and add to ul
-        $("ul").append("<li>" + taskText + "</li>");
+        $("ul").append("<li><span><i class = 'fa fa-trash'></i></span>" + taskText + "</li>")
     }
 });
-//Add tasks
+//fade input bar
+$(".fa-pencil").click(function(){
+    ("input[type='text']").fadeToggle()
+})
